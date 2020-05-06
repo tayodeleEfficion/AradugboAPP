bf0fdcb373a92480575105286a4e11d38f90bddb
daikage <davidkimekwu@gmail.com>
2020-05-06 04:16:41 +0100

    first commit

--- Screens/SigninScreen.jsx
+++ Screens/SigninScreen.jsx
@@ -38,6 +38,7 @@ const LoginScreen = ({ navigation }) => {
   const [name, setName] = React.useState(null);
   const [photo, setPhoto] = React.useState(null);
 
+   
   const signInWithGoogleAsync = async () => {
     try {
       const result = await Google.logInAsync({
@@ -51,6 +52,76 @@ const LoginScreen = ({ navigation }) => {
         setPhoto(result.user.photoUrl);
         console.log(result.user.photoUrl);
         setSignin(true);
+        const isUserEqual = (googleUser, firebaseUser) => {
+          if (firebaseUser) {
+            var providerData = firebaseUser.providerData;
+            for (var i = 0; i < providerData.length; i++) {
+              if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                  providerData[i].uid === googleUser.getBasicProfile().getId()) {
+                // We don't need to reauth the Firebase connection.
+                return true;
+              }
+            }
+          }
+          return false;
+        }
+         const onSignIn = (googleUser)=> {
+          console.log('Google Auth Response', googleUser);
+          // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+          var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+            unsubscribe();
+            // Check if we are already signed-in Firebase with the correct user.
+            if (!isUserEqual(googleUser, firebaseUser)) {
+              // Build Firebase credential with the Google ID token.
+              var credential = firebase.auth.GoogleAuthProvider.credential(
+                 googleUser.idToken,
+                 googleUser.accessToken,
+              );
+              // Sign in with credential from the Google user.
+              firebase
+              .auth().signInWithCredential(credential)
+              .then(function(result){
+                console.log('user signed in');
+                if(result.additionalUserInfo.isNewUser)
+                {
+      
+      
+              firebase
+              .database
+              .ref('/users'+ result.user.uid)
+              .set({
+                gmail: result.user.email,
+                profile_result: result.additionalUserInfo.profile.picture,
+                locale: result.additionalUserInfo.profile.locale,
+                first_name: result.additionalUserInfo.profile.given_name,
+                last_name: result.additionalUserInfo.profile.given_name,
+                created_at:Date.now()
+              })
+              .then(function(snapshot){
+              })
+            }else{
+              firebase
+                .database()
+                .ref('/users'+result.user.uid).update({
+                  last_logged_in:Date.now()
+                })
+            }
+      
+            
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+              });
+            } else {
+              console.log('User already signed-in Firebase.');
+            }
+          });
+        }
       } else {
         Alert.alert("Not found");
       }
